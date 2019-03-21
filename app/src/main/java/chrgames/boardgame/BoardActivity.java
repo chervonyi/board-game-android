195bd6bb9867e8f8a90d989f46126cf06b553df2
chervonyi <yuri.ternopil@gmail.com>
2019-03-21 12:11:12 -0500

    Create a project

---
+++ app/src/main/java/chrgames/boardgame/BoardActivity.java
@@ -0,0 +1,13 @@
+package chrgames.boardgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
